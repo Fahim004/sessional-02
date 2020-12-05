683a77a8bfa613b6d757bbabaaeed4b28ddc787d
Muhammad Fahim <maherfahim933@gmail.com>
2020-12-05 14:22:15 +0500

    SP17-BCS-037 create a new 2nd sassional project Add Splash Screen in App that Includes your name roll number and your Pick Now Add HomePage In the Project Now We Change Icon Add Two Level to run your app (Simple & Hard).

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,23 @@
 import 'package:flutter/material.dart';
-import 'HomePage.dart';
-import 'dart:async';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: MyApp(),
-      )
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: DicePage(),
+      ),
+    ),
   );
 }
 
-class MyApp extends StatefulWidget {
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  void initState(){
-    super.initState();
-    Future.delayed(Duration(seconds: 8),
-
-
-          (){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),
-        ),
-        );
-      },
-    );
-  }
-
+class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("M Fahim(SP17-BC-037)"),
-        //image: new Image.asset(
-          //  'assets/myPic.jpg'),
-
-
-
-
-      ),
-    );
+    return Container();
   }
 }
